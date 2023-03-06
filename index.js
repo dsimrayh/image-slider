607ba46ec0da0bd1245f22b384e86e805943ba1f
Damian <dsimrayh@yahoo.com>
2023-03-05 17:26:12 -0800

    styling finished
    finished styling all elements and got a handle on the necessary elements in index.js.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('handle');
+const [prevButtton, nextButton] = document.querySelectorAll('.arrow');
+const navCircles = document.querySelectorAll('.nav-circle');
+const image = document.querySelector('#image');
+
+prevButtton.addEventListener('click', () => {
+  console.log('previous image');
+});
+
+nextButton.addEventListener('click', () => {
+  console.log('next image');
+});
+
+navCircles.forEach((circle) => {
+  circle.addEventListener('click', () => {
+    console.log('nav-circle');
+  });
+});
+
+image.addEventListener('click', () => {
+  console.log('image');
+});
